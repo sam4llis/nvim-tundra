bfb54874ffc0132be64b169b5e0d3d976da7edb6
Sam Ellis <samwilliamellis@gmail.com>
2022-08-30 16:40:20 +0100

    chore: use `vim.tbl_get()`
    Remove square bracket syntax in place for Neovim `tbl_get()` API
    command. Though this makes the line length slightly longer, I think it
    makes the code far more readable.

--- lua/nvim-tundra/init.lua
+++ lua/nvim-tundra/init.lua
@@ -7,7 +7,7 @@ M.load = function()
   -- If the global variable `tundra_biome` is not set, then respect the user's
   -- background colour.
   if vim.g.tundra_biome == nil then
-    vim.g.tundra_biome = biomes[vim.opt.background:get()]
+    vim.g.tundra_biome = vim.tbl_get(biomes, vim.opt.background:get())
   end
 end
 
