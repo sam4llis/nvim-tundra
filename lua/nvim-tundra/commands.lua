59bcb538ad30fda0a90498ae8e2ca8f848f801b6
Sam Ellis <samwilliamellis@gmail.com>
2022-08-30 16:44:06 +0100

    feat: add `:Tundra` command
    The command `:Tundra` is effectively a wrapper around the `colorscheme`
    argument provided in Neovim. It will change the value of the global
    variable `tundra_biome` and then call `:colorscheme tundra`.
    
    The command takes one argument which is either the name of the biome (in
    this case, `alpine` or `arctic`) or a background colour (`light` or
    `dark`) and will change the highlight groups according to the argument
    added by the user.
    
    The first conditional check in the function ensures that the function
    fails on any key or value that is NOT found in the `biomes` table in
    `biomes.lua`.

---
+++ lua/nvim-tundra/commands.lua
@@ -0,0 +1,27 @@
+-- Imports.
+local biomes = require('nvim-tundra.biomes')
+
+-- A command to pick the theme used by nvim-tundra. The argument must equal a
+-- key in the `biomes` table (e.g., 'light') or a value in the `biomes` table
+-- (e.g., 'alpine') to work.
+vim.api.nvim_create_user_command('Tundra',
+  function(opts)
+    -- Check that the argument is a valid input.
+    if not (vim.tbl_contains(vim.tbl_values(biomes), opts.args) or vim.tbl_contains(vim.tbl_keys(biomes), opts.args)) then
+      vim.api.nvim_err_writeln('[nvim-tundra] Invalid biome name')
+      return
+    end
+
+    -- If the user specifies `light` or `dark` as the argument then convert it
+    -- to its respective biome name.
+    local biome = vim.tbl_get(biomes, opts.args)
+    if biome then
+      vim.g.tundra_biome = biome
+    else
+      vim.g.tundra_biome = opts.args
+    end
+
+    vim.cmd('colorscheme tundra')
+  end,
+  { nargs = 1 }
+)
---
+++ plugin/commands.lua
@@ -0,0 +1,1 @@
+require('nvim-tundra.commands')
