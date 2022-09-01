9cb672b76516c553e351b1876c1bbc1fa96b0058
Sam Ellis <samwilliamellis@gmail.com>
2022-09-01 18:14:33 +0100

    chore: unwrap comment

--- lua/nvim-tundra/init.lua
+++ lua/nvim-tundra/init.lua
@@ -5,8 +5,7 @@ local biomes = require('nvim-tundra.biomes')
 
 M.load = function()
 
-  -- If the global variable `tundra_biome` is not set, then respect the user's
-  -- background colour.
+  -- If the global variable `tundra_biome` is not set, then respect the user's background colour.
   if vim.g.tundra_biome == nil then
     vim.g.tundra_biome = vim.tbl_get(biomes, vim.opt.background:get())
   end
