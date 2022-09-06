sam4llis/nvim-tundra

commit 84e5d383e50713836b5771fafaaf0ed2869f1c9f

feat: add option to override built-in Tundra colours
Allows a user to specify in their setup function if they want any
default colours to be changed or overwritten. An example of this is
changing the built-in `orange._500` colour to white ('#FFFFFF'):

require('nvim-tundra').setup({
  -- ...
  overwrite = {
    colors = {
      orange = {
        _500 = '#FFFFFF',
      },
    },
  },
  -- ...
})

vim.opt.background = 'dark'
vim.cmd('colorscheme tundra')

lua/nvim-tundra/palette/arctic.lua
@@ -48,4 +48,5 @@ local cp = {
 
 }
 
-return cp
+-- Overwrite `cp` with any user configuration colours.
+return vim.tbl_extend('force', cp, vim.g.tundra_opts.overwrite.colors)

lua/nvim-tundra/base_configuration.lua
@@ -33,6 +33,9 @@ local base_configuration = {
     gitsigns = false,
     telescope = false,
   },
+  overwrite = {
+    colors = {},
+  },
 }
 
 return base_configuration

commit aa855d979905da2d96f1686ebf2b1a0132c2e3a9

fix: `deep_extend` overwrite configurations
A bug was introduced in commit 84e5d38 where adding a colour using the
user `overwrite` table caused the whole colour table to be overwritten.
This meant that if the colour contained different shades, and they were
not specified in the overwrite table, these colours would be
overwritten. Deep extending the table allows only the specific shade to
be added/overwritten, rather than the whole colour table.

lua/nvim-tundra/palette/arctic.lua
@@ -49,4 +49,4 @@ local cp = {
 }
 
 -- Overwrite `cp` with any user configuration colours.
-return vim.tbl_extend('force', cp, vim.g.tundra_opts.overwrite.colors)
+return vim.tbl_deep_extend('force', cp, vim.g.tundra_opts.overwrite.colors)