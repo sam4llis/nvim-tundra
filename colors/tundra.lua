e08d3970547d749a7a48624bdd4223c1ba0d0d86
Sam Ellis <samwilliamellis@gmail.com>
2022-09-11 16:49:43 +0100

    fix: set `termguicolors` to `true` within Tundra.
    Fixes issue #5 which caused the colorscheme to not be applied correctly
    if a user had not already set `termguicolors`.

--- colors/tundra.lua
+++ colors/tundra.lua
@@ -1,3 +1,6 @@
+-- Set `termguicolors` before loading the colorscheme.
+vim.opt.termguicolors = true
+
 -- Clear all highlight groups before loading the colorscheme.
 vim.cmd('highlight clear')
 require('nvim-tundra').load()
