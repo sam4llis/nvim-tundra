825ebb7bb56a466db95f326faf894f73462919cc
Sam Ellis <samwilliamellis@gmail.com>
2022-09-17 12:58:33 +0100

    feat: add `wezterm` ANSI support

---
+++ extras/wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+return {
+  colors = {
+    foreground = '#D1D5DB',
+    background = '#111827',
+
+    cursor_fg = '#111827',
+    cursor_bg = '#D1D5DB',
+    cursor_border = '#111827',
+
+    selection_fg = '#DDD6FE',
+    selection_bg = '#374151',
+
+    scrollbar_thumb = '#6B7280',
+    split = '#6B7280',
+
+    ansi = {
+      '#6B7280',
+      '#FCA5A5',
+      '#B1E3AD',
+      '#FBC19D',
+      '#BAE6FD',
+      '#957FB8',
+      '#DDD6FE',
+      '#A5F3FC',
+    },
+    brights = {
+      '#6B7280',
+      '#FCA5A5',
+      '#B1E3AD',
+      '#FBC19D',
+      '#BAE6FD',
+      '#938AA9',
+      '#DDD6FE',
+      '#A5F3FC',
+    },
+  },
+}
