f7808dd4845f97732ed6471c68094337f311a3af
Sam Ellis <samwilliamellis@gmail.com>
2022-11-18 22:20:18 +0000

    fix: remove redundant `:Tundra` commands
    They do nothing at the moment, but will be reintroduced in a later
    commit if a new theme is added.

--- lua/nvim-tundra/commands.lua
+++ lua/nvim-tundra/commands.lua
@@ -20,43 +20,6 @@ M.toggle_transparency = function(opts)
   })
 end
 
--- A function to pick the theme used by nvim-tundra. The argument must equal a
--- key in the `biomes` table (e.g., 'light') or a value in the `biomes` table
--- (e.g., 'alpine') to work. Can be called using the Tundra command line
--- interface via `:Tundra {arctic,dark,alpine,light}`.
-M.biome = function(opts)
-  if not (vim.tbl_contains(vim.tbl_values(biomes), opts.args) or vim.tbl_contains(vim.tbl_keys(biomes), opts.args)) then
-    vim.api.nvim_err_writeln('[nvim-tundra] Invalid biome name')
-    return
-  end
-
-  -- If the user specifies `light` or `dark` as the argument then convert it
-  -- to its respective biome name.
-  local biome = vim.tbl_get(biomes, opts.args)
-  if biome then
-    vim.g.tundra_biome = biome
-  else
-    vim.g.tundra_biome = opts.args
-  end
-
-end
-
-M.arctic = function(opts)
-  M.biome(opts)
-end
-
-M.alpine = function(opts)
-  M.biome(opts)
-end
-
-M.dark = function(opts)
-  M.biome(opts)
-end
-
-M.light = function(opts)
-  M.biome(opts)
-end
-
 vim.api.nvim_create_user_command('Tundra',
   function(opts)
     M[opts.args](opts)
@@ -69,7 +32,7 @@ vim.api.nvim_create_user_command('Tundra',
         function(value)
           return vim.startswith(value, line)
         end,
-        vim.tbl_values(vim.tbl_extend('force', biomes, { 'light', 'dark', 'toggle_dim', 'toggle_transparency' }))
+        vim.tbl_values(vim.tbl_extend('force', {}, { 'toggle_dim', 'toggle_transparency' }))
       )
     end,
   }
