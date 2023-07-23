f7977dd7a60cbca7c2e7807ccdb0d1cd16019edc
Sam Ellis <samwilliamellis@gmail.com>
2023-07-23 16:25:08 +0100

    chore: add all `neogit` highlight groups
    For now the groups that have not been implemented are commented.

--- lua/nvim-tundra/hl_group/plugins/neogit.lua
+++ lua/nvim-tundra/hl_group/plugins/neogit.lua
@@ -2,16 +2,84 @@ local M = {}
 
 M.map = function(ss, opts)
   return {
-    NeogitDiffContextHighlight = { bg = ss.cp.gray._875 },
+    -- Status buffer.
+    -- NeogitBranch = {},
+    -- NeogitRemote = {},
+    -- NeogitUnmergedInto = {},
+    -- NeogitUnpulledFrom = {},
+    -- NeogitObjectId = {},
+    -- NeogitStash = {},
+    -- NeogitFold = {},
 
-    NeogitDiffAdd = { fg = ss.cp.green._500, bg = ss.diff.add },
-    NeogitDiffAddHighlight = { link = 'NeogitDiffAdd' },
+    -- Status buffer items - git status.
+    -- NeogitChangeModified = {},
+    -- NeogitChangeAdded = {},
+    -- NeogitChangeDeleted = {},
+    -- NeogitChangeRenamed = {},
+    -- NeogitChangeUpdated = {},
+    -- NeogitChangeCopied = {},
+    -- NeogitChangeBothModified = {},
+    -- NeogitChangeNewFile = {},
 
-    NeogitDiffDelete = { fg = ss.git.removed, bg = ss.diff.delete.bg },
-    NeogitDiffDeleteHighlight = { link = 'NeogitDiffDelete' },
+    -- Status buffer section header.
+    -- NeogitUntrackedfiles = {},
+    -- NeogitUnstagedchanges = {},
+    -- NeogitUnmergedchanges = {},
+    -- NeogitUnpulledchanges = {},
+    -- NeogitRecentcommits = {},
+    -- NeogitStagedchanges = {},
+    -- NeogitStashes = {},
+    -- NeogitRebasing = {},
 
+    -- Signs (line highlights).
     NeogitHunkHeader = { link = 'diffLine' },
     NeogitHunkHeaderHighlight = { link = 'NeogitHunkHeader' },
+    -- NeogitDiffContext = {},
+    NeogitDiffContextHighlight = { bg = ss.cp.gray._875 },
+    NeogitDiffAdd = { fg = ss.cp.green._500, bg = ss.diff.add },
+    NeogitDiffAddHighlight = { link = 'NeogitDiffAdd' },
+    NeogitDiffDelete = { fg = ss.git.removed, bg = ss.diff.delete.bg },
+    NeogitDiffDeleteHighlight = { link = 'NeogitDiffDelete' },
+    -- NeogitDiffHeader = {},
+    -- NeogitDiffHeaderHighlight = {},
+    -- NeogitRebaseDone = {},
+    -- NeogitCursorLine = {},
+
+    -- Commit buffer.
+    -- NeogitFilePath = {},
+    -- NeogitCommitViewHeader = {},
+
+    -- Log view buffer.
+    -- NeogitGraph = {},
+    -- NeogitGraphBold = {},
+
+    -- Popups.
+    -- NeogitPopupSectionTitle = {},
+    -- NeogitPopupBranchName = {},
+    -- NeogitPopupBold = {},
+    -- NeogitPopupSwitchKey = {},
+    -- NeogitPopupOptionsKey = {},
+    -- NeogitPopupConfigKey = {},
+    -- NeogitPopupActionKey = {},
+    -- NeogitPopupSwitchDisabled = {},
+    -- NeogitPopupOptionsDisabled = {},
+    -- NeogitPopupConfigDisabled = {},
+    -- NeogitPopupActionDisabled = {},
+    -- NeogitPopupSwitchEnabled = {},
+    -- NeogitPopupOptionsEnabled = {},
+    -- NeogitPopupConfigEnabled = {},
+    -- NeogitPopupActionEnabled = {},
+
+    -- Notifications.
+    -- NeogitNotificationInfo = {},
+    -- NeogitNotificationWarning = {},
+    -- NeogitNotificationError = {},
+
+    -- Command history view.
+    -- NeogitCommandText = {},
+    -- NeogitCommandTime = {},
+    -- NeogitCommandCodeNormal = {},
+    -- NeogitCommandCodeError = {},
   }
 end
 
